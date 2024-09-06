6a53de75ac6fc359084a3cb59607d55ff49f7ba0
Arsal Naeem <arsalnaeem1@gmail.com>
2024-09-06 09:28:38 +0500

    added Vendor Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Customer from './Pages/Customer/Customer';
+import Vendors from './Pages/Vendors/Vendors';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/categories" element={<Dashboard />} />
         <Route path="/products" element={<Dashboard />} />
-        <Route path="/vendors" element={<Dashboard />} />
+        <Route path="/vendors" element={<Vendors />} />
         <Route path="/orders" element={<Dashboard />} />
         <Route path="/best-seller" element={<Dashboard />} />
         <Route path="/customer" element={<Customer />} />
