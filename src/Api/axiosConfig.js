5b6c06a9664e35bcfd8676122e9f79dc52cf0bad
Arsal Naeem <arsalnaeem1@gmail.com>
2024-09-15 20:30:50 +0500

    Add login feature

---
+++ src/Api/axiosConfig.js
@@ -0,0 +1,34 @@
+// src/api/axiosConfig.js
+import axios from "axios";
+import API_BASE_URL from "../constants";
+
+// Create an Axios instance with default configurations
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// A function to clear all interceptors
+const clearInterceptors = () => {
+  axiosInstance.interceptors.request.handlers = [];
+  axiosInstance.interceptors.response.handlers = [];
+};
+
+// A function to add the interceptor
+export const setupInterceptors = (token) => {
+  clearInterceptors(); // Clear existing interceptors
+
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      // If token is available, set the Authorization header
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+};
+
+export default axiosInstance;
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Login from "./Pages/Login/Login";
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Customer from './Pages/Customer/Customer';
 import Vendors from './Pages/Vendors/Vendors';
@@ -9,34 +10,41 @@ import Email from './Pages/Email/Email';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CustomerApproval from './Pages/CustomerApproval/CustomerApproval';
 import Discounts from './Pages/Discount/Discount';
-import Banner from './Pages/Banner/Banner'
+import Banner from './Pages/Banner/Banner';
 import AddBanner from './Pages/Banner/AddBanner';
 import Category from './Pages/Category/Category';
 import Notification from './Pages/Notification/NotificationForm';
 import Product from './Pages/Product/Product';
+import Error404 from './Pages/Error404/Error404';
+import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/categories" element={<Category />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/vendors" element={<Vendors />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/orders/:id" element={<OrdersDetails />} />
-        <Route path="/best-seller" element={<BestSeller />} />
-        <Route path="/customer" element={<Customer />} />
-        <Route path="/customer-approval" element={<CustomerApproval />} />
-        <Route path="/customer-approval" element={<CustomerApproval />} />
-        <Route path="/customer-roles" element={<Dashboard />} />
-        <Route path="/discounts" element={<Discounts />} />
-        <Route path="/email" element={<Email />} />
-        <Route path="/notice" element={<Notification />} />
-        <Route path="/banners" element={<Banner />} />
-        <Route path="/banners/add-banner" element={<AddBanner />} />
-
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Protected Routes */}
+        <Route path="/" element={<ProtectedRoute element={Dashboard} />} />
+        <Route path="/dashboard" element={<ProtectedRoute element={Dashboard} />} />
+        <Route path="/categories" element={<ProtectedRoute element={Category} />} />
+        <Route path="/products" element={<ProtectedRoute element={Product} />} />
+        <Route path="/vendors" element={<ProtectedRoute element={Vendors} />} />
+        <Route path="/orders" element={<ProtectedRoute element={Orders} />} />
+        <Route path="/orders/:id" element={<ProtectedRoute element={OrdersDetails} />} />
+        <Route path="/best-seller" element={<ProtectedRoute element={BestSeller} />} />
+        <Route path="/customer" element={<ProtectedRoute element={Customer} />} />
+        <Route path="/customer-approval" element={<ProtectedRoute element={CustomerApproval} />} />
+        <Route path="/customer-roles" element={<ProtectedRoute element={Dashboard} />} />
+        <Route path="/discounts" element={<ProtectedRoute element={Discounts} />} />
+        <Route path="/email" element={<ProtectedRoute element={Email} />} />
+        <Route path="/notice" element={<ProtectedRoute element={Notification} />} />
+        <Route path="/banners" element={<ProtectedRoute element={Banner} />} />
+        <Route path="/banners/add-banner" element={<ProtectedRoute element={AddBanner} />} />
+        
+        {/* Catch-all for non-existent routes */}
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
   );
