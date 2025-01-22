5a5ef516cf370adba7df90ecb222ab700ddc872b
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2025-01-22 19:35:39 +0500

    Campaign uncommented

--- src/Pages/Promotions/Campaign/Campaign.jsx
+++ src/Pages/Promotions/Campaign/Campaign.jsx
@@ -10,33 +10,37 @@ const { Option } = Select;
 const { Title } = Typography;
 
 const Campaign = () => {
-//   const [campaigns, setCampaigns] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [storeId, setStoreId] = useState("");
+  const [campaigns, setCampaigns] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [storeId, setStoreId] = useState("");
 
-//   const navigate = useNavigate();
-//   const retryRequest = useRetryRequest(); // Use the retry request hook
+  const navigate = useNavigate();
+  const retryRequest = useRetryRequest(); // Use the retry request hook
 
-//   // Function to fetch campaigns based on storeId
-//   const fetchCampaigns = async () => {
-//     setLoading(true);
-//     try {
-//       const response = await retryRequest(() =>
-//         axiosInstance.get(`/admin/campaigns/all-campaigns${storeId ? `?storeId=${storeId}` : ""}`)
-//       );
-//       setCampaigns(response.data.data);
-//     } catch (error) {
-//       console.error("Error fetching campaigns", error);
-//       message.error("Failed to fetch campaigns. Please try again.");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  // Function to fetch campaigns based on storeId
+  const fetchCampaigns = async () => {
+    setLoading(true);
+    try {
+      const response = await retryRequest(() =>
+        axiosInstance.get(
+          `/admin/campaigns/all-campaigns${
+            storeId ? `?storeId=${storeId}` : ""
+          }`
+        )
+      );
+      setCampaigns(response.data.data);
+    } catch (error) {
+      console.error("Error fetching campaigns", error);
+      message.error("Failed to fetch campaigns. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-//   // Fetch all campaigns on component mount
-//   useEffect(() => {
-//     fetchCampaigns();
-//   }, []); // Only run once when component mounts
+  // Fetch all campaigns on component mount
+  useEffect(() => {
+    fetchCampaigns();
+  }, []); // Only run once when component mounts
 
   // Redirect to the edit page for the selected campaign
   const handleEdit = (id) => {
@@ -47,8 +51,8 @@ const Campaign = () => {
   const handleDelete = async (id) => {
     try {
       //console.log(id);
-      await retryRequest(() =>
-        axiosInstance.delete(`/admin/delete-campaigns/${id}`) // Adjust the endpoint as needed
+      await retryRequest(
+        () => axiosInstance.delete(`/admin/delete-campaigns/${id}`) // Adjust the endpoint as needed
       );
       message.success("Campaign deleted successfully.");
       fetchCampaigns(); // Refresh the campaign list after deletion
@@ -75,7 +79,7 @@ const Campaign = () => {
       title: "Planned Date Of Sending", // Changed column title
       dataIndex: "DontSendBeforeDateUtc",
       key: "dontSendBefore",
-      render: (text) => text ? dayjs(text).format("YYYY-MM-DD HH:mm:ss") : "", // Format date or return empty string
+      render: (text) => (text ? dayjs(text).format("YYYY-MM-DD HH:mm:ss") : ""), // Format date or return empty string
     },
     {
       title: "Actions",
@@ -91,7 +95,7 @@ const Campaign = () => {
             okText="Yes"
             cancelText="No"
           >
-            <Button type="link" style={{ color: 'red' }}>
+            <Button type="link" style={{ color: "red" }}>
               Delete
             </Button>
           </Popconfirm>
@@ -100,42 +104,50 @@ const Campaign = () => {
     },
   ];
 
-//   // Handle StoreId change
-//   const handleStoreIdChange = (value) => {
-//     setStoreId(value);
-//   };
+  //   // Handle StoreId change
+  const handleStoreIdChange = (value) => {
+    setStoreId(value);
+  };
 
-//   return (
-//     <CustomLayout pageTitle="Campaigns" menuKey="15">
-//       <Title level={3} style={{ marginBottom: '16px', textAlign:'center' }}>Campaign Management</Title>
-//       <Button type="primary" onClick={() => navigate('/campaign/create-new')} style={{ marginBottom: '16px' }}>
-//         Create New Campaign
-//       </Button>
-//       <div style={{ marginBottom: "16px", display: 'flex', alignItems: 'center' }}>
-//         <Select
-//           placeholder="Select Store ID"
-//           value={storeId}
-//           onChange={handleStoreIdChange}
-//           style={{ width: 200, marginRight: 8 }}
-//         >
-//           <Option value="">All</Option>
-//           <Option value="1">Sky Glass</Option>
-//           <Option value="3">Sky Blue Wholesale</Option>
-//         </Select>
-//         <Button type="primary" onClick={fetchCampaigns} loading={loading}>
-//           Search Campaigns
-//         </Button>
-//       </div>
-//       <Table
-//         dataSource={campaigns}
-//         columns={columns}
-//         loading={loading}
-//         scroll={{ x: "max-content" }}
-//         rowKey="Id"
-//         pagination={{ pageSize: 15 }} // Add pagination
-//       />
-//     </CustomLayout>
-//   );
+  return (
+    <CustomLayout pageTitle="Campaigns" menuKey="15">
+      <Title level={3} style={{ marginBottom: "16px", textAlign: "center" }}>
+        Campaign Management
+      </Title>
+      <Button
+        type="primary"
+        onClick={() => navigate("/campaign/create-new")}
+        style={{ marginBottom: "16px" }}
+      >
+        Create New Campaign
+      </Button>
+      <div
+        style={{ marginBottom: "16px", display: "flex", alignItems: "center" }}
+      >
+        <Select
+          placeholder="Select Store ID"
+          value={storeId}
+          onChange={handleStoreIdChange}
+          style={{ width: 200, marginRight: 8 }}
+        >
+          <Option value="">All</Option>
+          <Option value="1">Sky Glass</Option>
+          <Option value="3">Sky Blue Wholesale</Option>
+        </Select>
+        <Button type="primary" onClick={fetchCampaigns} loading={loading}>
+          Search Campaigns
+        </Button>
+      </div>
+      <Table
+        dataSource={campaigns}
+        columns={columns}
+        loading={loading}
+        scroll={{ x: "max-content" }}
+        rowKey="Id"
+        pagination={{ pageSize: 15 }} // Add pagination
+      />
+    </CustomLayout>
+  );
 };
 
 export default Campaign;
