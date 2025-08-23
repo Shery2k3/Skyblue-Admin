f6295725cdf802d1564951359f3850f87cfe49db
Shahryar Khan <68807832+Shery2k3@users.noreply.github.com>
2025-08-23 06:45:21 +0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-const API_BASE_URL = "http://localhost:3000"
+// const API_BASE_URL = "http://localhost:3000"
+// const API_BASE_URL = "https://skyblue-backend-production.up.railway.app"
+// const API_BASE_URL = "http://104.129.128.106:3000" // Kamatera VPS
+const API_BASE_URL = "https://v2.skybluewholesale.com" // Domain from IONOS on Contabo VPS
+
 export default API_BASE_URL;
